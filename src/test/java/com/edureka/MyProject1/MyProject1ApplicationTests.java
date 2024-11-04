c4d700475b1286d4f55ddcfb91434dd569fccce3
msh-r <hramim389@gmail.com>
2024-11-03 19:12:51 -0500

    added files

---
+++ src/test/java/com/edureka/MyProject1/MyProject1ApplicationTests.java
@@ -0,0 +1,29 @@
+package com.edureka.MyProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProject1ApplicationTests {
+
+	@Test
+	void tc1() {
+		System.out.println("from tc1");
+	}
+	@Test
+	void tc2() {
+		System.out.println("from tc2");
+	}
+	@Test
+	void tc3() {
+		System.out.println("from tc3");
+	}
+	@Test
+	void tc4() {
+		System.out.println("from tc4");
+	}
+	@Test
+	void tc5() {
+		System.out.println("from tc5");
+	}
+}
